5d6fa7830b92ae126d3899e65da9c428d980aff9
BORHAN <borhanuddin6888@gmail.com>
2022-06-19 15:50:20 +0600

    second commit

---
+++ src/features/posts/PostsView.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchPosts } from './postSlice';
+
+const PostsView = () => {
+  const { isLoading, posts, error } = useSelector((state) => state.posts);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchPosts());
+  }, []);
+
+  return (
+    <div>
+      {isLoading && <h3>Loading...</h3>}
+      {error && <h3>{error}</h3>}
+      {posts &&
+        posts.map((post) => {
+          return (
+            <section>
+              <article>
+                <h3>{post.title}</h3>
+                <p>{post.body}</p>
+              </article>
+            </section>
+          );
+        })}
+    </div>
+  );
+};
+
+export default PostsView;
